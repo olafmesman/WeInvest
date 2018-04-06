d0d48db5c8b512f2617043c3b32c6344aa58a0a2
Joseph Lloyd <joe.lloyd.22.24@gmail.com>
2018-04-07 01:46:22 +0200

    added quick register page

---
+++ components/register.php
@@ -0,0 +1,23 @@
+<div class="login-wrap">
+	<div class="login">
+
+		<div class="mdc-text-field" data-mdc-auto-init="MDCTextField">
+			<input type="text" class="mdc-text-field__input" id="email-input">
+			<label for="demo-input" class="mdc-floating-label">Email</label>
+		</div>
+
+		<div class="mdc-text-field" data-mdc-auto-init="MDCTextField">
+			<input type="password" class="mdc-text-field__input" id="password-input">
+			<label for="demo-input" class="mdc-floating-label">Password</label>
+		</div>
+
+		<div class="login-button">
+			<button class="mdc-button">Register</button>
+		</div>
+
+		<div class="register-link">
+			<span>Have an account? <a href="/login">Login</a></span>
+		</div>
+
+	</div>
+</div>
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
 import '@babel/polyfill';
 
 import MdcInputModule from './modules/mdc-input';
-//import MdcButtonModule from './modules/mdc-button';
 import ExampleModule from './modules/example';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -16,12 +15,5 @@ document.addEventListener('DOMContentLoaded', () => {
 		const controller = new MdcInputModule(input);
 		controller.bootstrap();
 	}
-	//
-	//const buttons = document.querySelectorAll('.mdc-button');
-	//
-	//for (const button of buttons) {
-	//	const controller = new MdcButtonModule(button);
-	//	controller.bootstrap();
-	//}
 
 });
