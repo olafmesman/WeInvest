25bf0762caa321634d2fb6c3af35e45e8635388b
Joseph Lloyd <joe.lloyd.22.24@gmail.com>
2018-04-06 22:47:42 +0200

    added routing and css

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+//  require_once('screens/home.php');
+
+// Grabs the URI and breaks it apart in case we have querystring stuff
+$request_uri = explode('?', $_SERVER['REQUEST_URI'], 2);
+
+// Route it up!
+switch ($request_uri[0]) {
+	// Home page
+	case '/':
+		require './screens/home.php';
+		break;
+	// Login page
+	case '/login':
+		require './screens/login.php';
+		break;
+	// Everything else
+	default:
+		header('HTTP/1.0 404 Not Found');
+		require './screens/404.php';
+		break;
+}
