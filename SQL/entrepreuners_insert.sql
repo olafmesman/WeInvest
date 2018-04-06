b1e6d9a7a74449410b00fa32e4e9f64da953847b
Unknown <thomas.van.rijsselt@gmail.com>
2018-04-07 01:35:36 +0200

    gitflow-feature-stash: entrepreneursinsert
    inserted fake 6 entrepreneurs and 1 investor

---
+++ SQL/entrepreuners_insert.sql
@@ -0,0 +1,40 @@
+<?php
+
+USE WeInvest;
+
+INSERT INTO entrepreneurs_profile (description, profile_picture_url, logo_url) VALUES ('Bismart is a leading Kenyan insurance aggregator, leveraging on digital platforms and technologies to connect our customers to the best insurance and investments solutions available in the market. It is regulated by Insurance regulatory authority. We provide our clients with education and the ability to compare and buy premier insurance and investment solutions at the convenience of their palms.', 'user_1.png', 'logo_1.png');
+INSERT INTO entrepreneurs (email, password, first_name, last_name, entrepreneur_profiles_id) VALUES ('eunice@gmail.com', 'asdf', 'Eunice', 'Maina Mburu', 1);
+
+
+INSERT INTO entrepreneurs_profile (description, profile_picture_url, logo_url) VALUES ();
+INSERT INTO entrepreneurs (email, password, first_name, last_name, entrepreneur_profiles_id) VALUES ();
+
+
+
+INSERT INTO entrepreneurs_profile (description, profile_picture_url, logo_url) VALUES ('A business financial technology integration company building financial backbone systems & infrastructure that enables secure and efficient movements of transactions whiles positively impacting lives. To deliver value, BlueSPACE has partnered with the following innovation vendors to make this a success. With our trained set of sales and pre-sales consultants, BlueSPACE is bringing real difference in business technology integration. Our value add is in our people and our approach.', 'user_2.png', 'logo_2.png');
+INSERT INTO entrepreneurs (email, password, first_name, last_name, entrepreneur_profiles_id) VALUES ('samtettey@gmail.com', 'asdf', 'Sam', 'Tettey', 1);
+
+
+INSERT INTO entrepreneurs_profile (description, profile_picture_url, logo_url) VALUES ('Interpay is an electronic payment platform that makes transactions and e-commerce simpler, secure, and more convenient. We do this by making it possible for merchants and consumers to make and receive payments of bills, invoices, and fees
+.', 'user_3.png', 'logo_3.png');
+INSERT INTO entrepreneurs (email, password, first_name, last_name, entrepreneur_profiles_id) VALUES ('saqibnazir@gmail.com', 'asdf', 'Saqib', 'Nazir', 1);
+
+
+INSERT INTO entrepreneurs_profile (description, profile_picture_url, logo_url) VALUES ('Discover local restaurants that deliver to your doorstep. Jumia food is Ghana’s most convenient online food ordering site, connecting people with over 90 of Accra’s best restaurants around them
+.', 'user_4.png', 'logo_4.png');
+INSERT INTO entrepreneurs (email, password, first_name, last_name, entrepreneur_profiles_id) VALUES ('charlesowusu@gmail.com', 'asdf', 'Charles', 'Owusu', 1);
+
+
+INSERT INTO entrepreneurs_profile (description, profile_picture_url, logo_url) VALUES ('Data Recovery Solutions IP Telephony Solutions Network Switching Solutions Power Management Network Security Solutions Video Conferencing Routing Solutions Wireless Solutions Identity Acess Management Storage Solutions Remote Acess Solutions.', 'user_5.png', 'logo_5.png');
+INSERT INTO entrepreneurs (email, password, first_name, last_name, entrepreneur_profiles_id) VALUES ('hannaowusu@gmail.com', 'asdf', 'Hanna', 'Owusu', 1);
+
+
+INSERT INTO entrepreneurs_profile (description, profile_picture_url, logo_url) VALUES ('This portal enables individuals owning businesses to access Government to business (G2B) services online. Through a single account, a business will be able to access services such as business licences, permits and registrations offered by different government departments conviniently.
+.', 'user_6.png', 'logo_6.png');
+INSERT INTO entrepreneurs (email, password, first_name, last_name, entrepreneur_profiles_id) VALUES ('kyraelhalabi@gmail.com', 'asdf', 'Kyra', 'El Halabi', 1);
+
+
+INSERT INTO investor (email, password, first_name, last_name) VALUES ('test@gmail.com', 'asdf', 'test', 'test');
+
+
+?>
