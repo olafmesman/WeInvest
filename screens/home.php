6bc410afa462f1c1da133f779bf031b3d825c6a5
Tea <teateearu@gmail.com>
2018-04-07 00:43:27 +0200

    Start header

--- screens/home.php
+++ screens/home.php
@@ -4,7 +4,21 @@
 <body>
 
 <main>
-	<h1>I am the home page</h1>
+	<header class="mdc-top-app-bar">
+	  <div class="mdc-top-app-bar__row">
+	    <section class="mdc-top-app-bar__section" role="toolbar">
+	      <a href="#" class="material-icons mdc-top-app-bar__action-item" aria-label="Home" alt="Home">Home</a>
+	      <a href="#" class="material-icons mdc-top-app-bar__action-item" aria-label="Matches" alt="Matches">Matches</a>
+	      <a href="#" class="material-icons mdc-top-app-bar__action-item" aria-label="Explore" alt="Explore">Explore</a>
+	      <a href="#" class="material-icons mdc-top-app-bar__action-item" aria-label="Notifications" alt="">Notifications</a>
+	    </section>
+	    <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
+	      <a href="#" class="material-icons mdc-top-app-bar__navigation-icon">Menu</a>
+	      <!-- <span class="mdc-top-app-bar__title">Title</span> -->
+	    </section>
+	  </div>
+	</header>
+
 </main>
 
 <script src="/dist/app.js"></script>
