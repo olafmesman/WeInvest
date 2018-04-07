b7a37cca06fd3ac5f785ba2f3004b0493ee8dcc6
Unknown <thomas.van.rijsselt@gmail.com>
2018-04-07 04:39:14 +0200

    /login_logic: Auto stash before merge of "feature/login_logic" and "origin/feature/login_logic"

--- SQL/posts_insert.sql
+++ SQL/posts_insert.sql
@@ -1,6 +1,10 @@
-USE WeInvest;
+INSERT INTO posts (content, image_url, entrepreneurs_id) VALUES ('Jumia food on NBS Live. Now 90 of the local restaurants are available throughout this ordering service.', 'user_url', 1);
 
-INSERT INTO posts (content, image_url, entrepreneur_id) VALUES ("We met up with Mark rutte.", "post_1.jpg", 1);
-INSERT INTO posts (content, image_url, entrepreneur_id) VALUES ("We met up with Mark rutte 2.", "post_2.jpg", 1);
-INSERT INTO posts (content, image_url, entrepreneur_id) VALUES ("We love our comminuty.", "post_3.jpg", 1);
-INSERT INTO posts (content, image_url, entrepreneur_id) VALUES ("We won!", "post_4.jpg", 1);
+INSERT INTO posts (content, image_url, entrepreneurs_id) VALUES ('Fahad Awadhv, founder of FZTX-Technologies, was one of the 10 speakers during the annual Youth Entrepreneurs – Event in Nairobi', 'user_2.png', 1);
+
+INSERT INTO posts (content, image_url, entrepreneurs_id) VALUES ('Forty-four countries sign historic African Union free trade agreement', 'user_3.png', 1);
+
+INSERT INTO posts (content, image_url, entrepreneurs_id) VALUES ('Our CEO, Ezekiel Nwankwo, addressing the problem of infrastructure planning during the 2018 Innovation Summit in Nairobi.', 'user_4.png', 1);
+
+INSERT INTO posts (content, image_url, entrepreneurs_id) VALUES ('The first EY Hackathon Kenya is a fact. The winning team will be entering WeInvest on August 1st.
+40% of the hackers where provided from out the WeInvest-community.', 'user_4.png', 2);
