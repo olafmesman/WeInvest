226e7464b7ef82d9e45bb8605c47e0fb524e525c
Tea <teateearu@gmail.com>
2018-04-07 08:22:28 +0200

    Started feed

---
+++ screens/feed.php
@@ -0,0 +1,23 @@
+<html>
+<?php include 'components/head.php'; ?>
+<body>
+<?php include 'components/header.php'; ?>
+<main>
+	<body>
+	  <div class="general-wrapper-feed">
+	    <div class="component-wrapper">
+	      <h1>Your exclusive peer-group</h1>
+	      <p>Here you can see the entrepreneurs that fit your criteria</p>
+				<button class="mdc-button" href="/matchmaker">Edit</button>
+	  </div>
+	  </div>
+	</body>
+	<?php include 'components/feed-user-block.php'; ?>
+  <?php include 'components/feed-user-block.php'; ?>
+  <?php include 'components/feed-user-block.php'; ?>
+
+</main>
+
+<script src="/dist/app.js"></script>
+</body>
+</html>
